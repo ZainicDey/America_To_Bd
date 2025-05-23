5feeca7054dc3affe1fe4c980af46cb256d96368
Anik Dey <ihanik.ad@gmail.com>
2025-05-23 12:06:48 +0600

    take request without slash

--- americatobd/settings.py
+++ americatobd/settings.py
@@ -63,7 +63,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    # 'URL_TRAILING_SLASH': False,
 }
+APPEND_SLASH = False
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class MannualPayment(models.Model):
+    tracker = models.CharField(max_length=15)
+    bank_name = models.CharField(max_length=100)
+    transaction_id = models.CharField(max_length=100)
