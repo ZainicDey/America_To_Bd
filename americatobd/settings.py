d2f6798e0ffb0ecd818537b9e6e81604641f412d
Anik Dey <ihanik.ad@gmail.com>
2025-06-25 13:52:28 +0600

    rest ui removed

--- americatobd/settings.py
+++ americatobd/settings.py
@@ -65,6 +65,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
