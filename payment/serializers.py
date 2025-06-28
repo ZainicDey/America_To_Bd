5d51b4a28fda2ed63a42afdc7fa478bbfaa87f71
Anik Dey <ihanik.ad@gmail.com>
2025-06-28 16:31:40 +0600

    order id added for manual payment

--- payment/serializers.py
+++ payment/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import MannualPayment
-
+from order.models import ResolvedOrder
 class MannualPaymentSerializer(serializers.ModelSerializer):
+    order_id = serializers.SerializerMethodField()
     class Meta:
         model = MannualPayment
         fields = '__all__'
@@ -10,3 +11,11 @@ class MannualPaymentSerializer(serializers.ModelSerializer):
             'image': {'required': False},
             'public_id': {'required': False}
         }
+        read_only_fields = ('created_at', 'updated_at,  order_id')
+
+    def get_order_id(self, obj):
+        order = ResolvedOrder.objects.filter(tracker=obj.tracker).first()
+        if order:    
+            return order.id
+        else :
+            return None
