41b99b502687a4c398ba23603b8dd0b0e7ae100e
Anik Dey <ihanik.ad@gmail.com>
2025-07-02 00:31:27 +0600

    jomashop added

--- automation/jomashop.py
+++ automation/jomashop.py
@@ -1,30 +1,30 @@
-# from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright
 
-# def main(url):
-#     with sync_playwright() as p:
-#         browser = p.chromium.launch(headless=True) 
-#         page = browser.new_page()
-#         page.goto(url)
+def main(url):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True) 
+        page = browser.new_page()
+        page.goto(url)
         
-#         title = page.title()
+        title = page.title()
 
-#         # Grab the price text inside the first span within .now-price div
-#         price = page.locator("div.now-price > span").first.text_content()
+        # Grab the price text inside the first span within .now-price div
+        price = page.locator("div.now-price > span").first.text_content()
 
-#         # Grab the image src attribute of the img with id 'product-main-image-gallery'
-#         img_src = page.locator("img#product-main-image-gallery").get_attribute("src")
+        # Grab the image src attribute of the img with id 'product-main-image-gallery'
+        img_src = page.locator("img#product-main-image-gallery").get_attribute("src")
 
-#         if price and img_src and title:
-#             product_data = {
-#                 "title": title,
-#                 "image": img_src,
-#                 "price": price
-#             }
-#             browser.close()
-#             return product_data
-#         browser.close()
-#         return None
+        if price and img_src and title:
+            product_data = {
+                "title": title,
+                "image": img_src,
+                "price": price
+            }
+            browser.close()
+            return product_data
+        browser.close()
+        return None
         
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
