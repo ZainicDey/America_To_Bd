88bf03e7668411482b4548f2b15d12ff65674bf1
Anik Dey <ihanik.ad@gmail.com>
2025-07-03 00:56:49 +0600

    cors added for new backend

--- americatobd/settings.py
+++ americatobd/settings.py
@@ -99,6 +99,13 @@ CCORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "https://americatobd.com",
     "https://www.americatobd.com",
+    "https://starfish-app-9bx4b.ondigitalocean.app",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://starfish-app-9bx4b.ondigitalocean.app",
+    "https://americatobd.com",
+    "https://www.americatobd.com",
 ]
 
 CORS_ALLOW_METHODS = ["DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT"]
