ede7bf3a3da2d2e88d7f38441bafd4fe09522e82
Anik Dey <ihanik.ad@gmail.com>
2025-07-06 12:53:41 +0600

    detailed list of automatic order

--- automation/serializers.py
+++ automation/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from . import models
-
+from userrole.serializers import UserSerializer, AddressSerializer
 class AutomationSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    address = AddressSerializer(read_only=True)
     class Meta:
         model = models.AutomatedOrder
         fields = '__all__'
+        read_only_fields = ['id', 'user', 'address']
 
